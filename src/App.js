6f8cab79215c41c67537bc9cb29595172217cc9a
PTBYSR <paul.emechebe@gmail.com>
2022-04-04 03:43:55 +0100

    framer-motion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
       <h1> portfolio </h1>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        onClick={() => null}
+        >
+        press me
+      </motion.button>
     </div>
   );
 }
