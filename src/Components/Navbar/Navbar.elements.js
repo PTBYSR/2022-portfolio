5d81b3ae8fcd8583e5df66fc684ecdf53e2d03df
PTBYSR <paul.emechebe@gmail.com>
2022-04-10 02:34:57 +0100

    chore

---
+++ src/Components/Navbar/Navbar.elements.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+    height: 80px;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 0;
+    z-index: 999;
+`;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
-
 import React from 'react'
+import { Nav } from './Navbar.elements'
+
 
 function NavBar() {
   return (
-    <nav>
+    <nav className='flex bg-light'>
       <ul className="primary-navigation underline-indicators flex">
-          <li className="active"><a className="fs-secondary" href="#">Home</a></li>
-          <li><a className="fs-secondary text-light" href="#">Work</a></li>
-          <li><a className="fs-secondary text-light" href="#">About</a></li>
-          <li><a className="fs-secondary text-light" href="#">Resume</a></li>
+          <li className="active"><a className="fs-secondary text-dark" href="#">Home</a></li>
+          <li><a className="fs-secondary text-dark" href="#">Work</a></li>
+          <li><a className="fs-secondary text-dark" href="#">About</a></li>
+          <li><a className="fs-secondary text-dark" href="#">Resume</a></li>
       </ul>
     </nav>
   )
