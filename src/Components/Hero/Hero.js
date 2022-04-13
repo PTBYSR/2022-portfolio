f295029a5b51635162e7647c7ee837b283d012ed
PTBYSR <paul.emechebe@gmail.com>
2022-04-13 03:32:15 +0100

    chore-hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -10,9 +10,10 @@ function Hero() {
   return (
     <>
       <Landing className='bg-light flex container'>
-        <LandingCardOne className="ff-mont fw-semibold fs-heading">
-          <h1>Designer,</h1>
-          <h1>Developer</h1>
+        <LandingCardOne className="ff-mont">
+          <h1 className="fw-semibold fs-heading">Designer,</h1>
+          <h1 className="fw-semibold fs-heading">Developer</h1>
+          <p>creating breathe-taking interfaces is what i love doing</p>
         </LandingCardOne>
         <LandingCardTwo />
         <LandingCardThree className="flex ff-mont fw-semibold fs-heading">
