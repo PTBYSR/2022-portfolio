40df2fe4eaba8bac3e90d0cb6a4514fbf36f2114
PTBYSR <paul.emechebe@gmail.com>
2022-04-14 21:44:45 +0100

    chore-hero

--- src/Components/Hero/Hero.element.js
+++ src/Components/Hero/Hero.element.js
@@ -1,27 +1,23 @@
-import styled from 'styled-components';
-import { helpers } from '../StyleHelpers'
-
-
+import styled from "styled-components";
+import { helpers } from "../StyleHelpers";
 
 export const Landing = styled.section`
-    width: 100%;
-    height: 100vh;
-    gap: 0rem;
-    /* margin-top: 30px; */
-    padding-top: ${props => props.theme.main};
+  width: 100%;
+  height: 100vh;
+  gap: 0rem;
+  /* margin-top: 30px; */
+  padding-top: ${(props) => props.theme.main};
 `;
 export const LandingCardOne = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 export const LandingCardTwo = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 export const LandingCardThree = styled.div`
-    ${helpers};
-    width: 100%;
-    flex-direction: column;
-    align-items: end;
-    justify-content: space-between;
+  width: 100%;
+  flex-direction: column;
+  align-items: end;
+  justify-content: space-between;
 `;
-export const NameLabel = styled.div`
-`
+export const NameLabel = styled.div``;
