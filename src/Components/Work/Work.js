6eb26f12d5758292f9e2606edf885a57cc068ed2
PTBYSR <paul.emechebe@gmail.com>
2022-04-14 21:58:47 +0100

    starting-work-page

---
+++ src/Components/Work/Work.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Landing } from '../Hero/Hero.element'
+
+function Work() {
+  return (
+    <>
+    <Landing>
+
+    </Landing>
+    </>
+  )
+}
+
+export default Work
