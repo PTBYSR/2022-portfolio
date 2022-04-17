78a35be1d1f4cc2813eb93f26eddf3eed19e5660
PTBYSR <paul.emechebe@gmail.com>
2022-04-17 23:40:57 +0100

    project-section

--- src/Components/Work/ProjectSection.element.js
+++ src/Components/Work/ProjectSection.element.js
@@ -2,4 +2,10 @@ import styled from 'styled-components';
 
 export const ProjectSectionWrapper = styled.div`
 
-`
+`;
+export const ImageWrapper = styled.div`
+
+`;
+export const TextWrapper = styled.div`
+
+`;
--- src/Components/Work/ProjectSection.js
+++ src/Components/Work/ProjectSection.js
@@ -1,12 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import { 
+    ProjectSectionWrapper,
+    ImageWrapper,
+    TextWrapper
+ } from './ProjectSection.element';
 
 
 function ProjectSection() {
   return (
     <>
         <ProjectSectionWrapper>
-
+            <ImageWrapper></ImageWrapper>
+            <TextWrapper></TextWrapper>
         </ProjectSectionWrapper>
     </>
   )
