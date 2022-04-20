892a58ea94901c131793234a54b729830f70720f
PTBYSR <paul.emechebe@gmail.com>
2022-04-20 19:10:25 +0100

    about-section

--- src/Components/About/About.element.js
+++ src/Components/About/About.element.js
@@ -4,13 +4,7 @@ export const AboutSection = styled.section`
     width: 100%;
     height: 400px;
     padding-top: ${(props) => props.theme.main};
-`
-// AboutSection,
-//AboutHeading,
-//Image,
-//TextSection,
-//AboutMain,
-//AboutSecondary,
+`;
 export const AboutHeading = styled.div`
 `;
 export const Image = styled.div`
